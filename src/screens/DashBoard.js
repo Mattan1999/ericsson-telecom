f8096045905c26c14c31b7ad794680c3ee63c8d4
ChabbeH <72022949+ChabbeH@users.noreply.github.com>
2021-11-21 17:11:11 +0100

    removing errors

--- src/screens/DashBoard.js
+++ src/screens/DashBoard.js
@@ -3,28 +3,25 @@ import { SafeAreaView, Text, StyleSheet } from "react-native";
 import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 
 const DashBoard = () => {
-  const [connectionType, setConnectionType] = useState(null);
-  const [details, setDetails] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [userLocation, setUserLocation] = useState(null);
-  const [heatMapPoints, setheatMapPoints] = useState([]);
-
   const netInfo = useNetInfo();
 
   return (
     <SafeAreaView>
       <Text>Connection type: {netInfo.type}</Text>
-      <Text>SSID: {netInfo.details.ssid}</Text>
-      <Text>BSSID: {netInfo.details.bssid}</Text>
-      <Text>IP-Adress: {netInfo.details.ipAddress}</Text>
-      <Text>Subnet Mask: {netInfo.details.subnet}</Text>
-      <Text>Signal strength: {netInfo.details.strength}</Text>
-      <Text>Carrier name: {netInfo.details.carrier}</Text>
-      <Text>Cellulur generation: {netInfo.details.cellularGeneration}</Text>
-      <Text>Network Frequency: {netInfo.details.frequency}</Text>
-      <Text>
-        Expensive connection: {netInfo.details.isConnectionExpensive.toString()}
-      </Text>
+      <Text>SSID: {netInfo.details?.ssid}</Text>
+      <Text>BSSID: {netInfo.details?.bssid}</Text>
+      <Text>IP-Adress: {netInfo.details?.ipAddress}</Text>
+      <Text>Subnet Mask: {netInfo.details?.subnet}</Text>
+      <Text>Signal strength: {netInfo.details?.strength}</Text>
+      <Text>Carrier name: {netInfo.details?.carrier}</Text>
+      <Text>Cellulur generation: {netInfo.details?.cellularGeneration}</Text>
+      <Text>Network Frequency: {netInfo.details?.frequency}</Text>
+      {netInfo.details?.isConnectionExpensive !== undefined && (
+        <Text>
+          Expensive connection:{" "}
+          {netInfo.details?.isConnectionExpensive.toString()}
+        </Text>
+      )}
     </SafeAreaView>
   );
 };
