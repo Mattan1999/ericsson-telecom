6b6aaa0e305231c482f797c1b8e7701d76c08f6c
Cassandra.Holmquist <Cassandra.Holmquist@sigma.se>
2021-11-24 10:58:29 +0100

    Started to add some styling

--- src/screens/DashBoard.js
+++ src/screens/DashBoard.js
@@ -1,31 +1,72 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { SafeAreaView, Text, StyleSheet, View } from "react-native";
 import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 
 const DashBoard = () => {
   const netInfo = useNetInfo();
 
   return (
-    <SafeAreaView>
-      <Text>Connection type: {netInfo.type}</Text>
-      <Text>SSID: {netInfo.details?.ssid}</Text>
-      <Text>BSSID: {netInfo.details?.bssid}</Text>
-      <Text>IP-Adress: {netInfo.details?.ipAddress}</Text>
-      <Text>Subnet Mask: {netInfo.details?.subnet}</Text>
-      <Text>Signal strength: {netInfo.details?.strength}</Text>
-      <Text>Carrier name: {netInfo.details?.carrier}</Text>
-      <Text>Cellulur generation: {netInfo.details?.cellularGeneration}</Text>
-      <Text>Network Frequency: {netInfo.details?.frequency}</Text>
-      {netInfo.details?.isConnectionExpensive !== undefined && (
-        <Text>
-          Expensive connection:{" "}
-          {netInfo.details?.isConnectionExpensive.toString()}
-        </Text>
-      )}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.dashboard}>
+        <View style={styles.firstColumn}>
+          <Text>Connection type: </Text>
+          <Text>SSID: </Text>
+          <Text>BSSID: </Text>
+          <Text>IP-Adress: </Text>
+          <Text>Subnet Mask: </Text>
+          <Text>Signal strength: </Text>
+          <Text>Carrier name: </Text>
+          <Text>Cellular generation: </Text>
+          <Text>Network Frequency: </Text>
+          {netInfo.details?.isConnectionExpensive !== undefined && (
+            <Text>Expensive connection:{" "}</Text>
+          )}
+        </View>
+        <View style={styles.secondColumn}>
+          <Text>{netInfo.type}</Text>
+          <Text>{netInfo.details?.ssid}</Text>
+          <Text>{netInfo.details?.bssid}</Text>
+          <Text>{netInfo.details?.ipAddress}</Text>
+          <Text>{netInfo.details?.subnet}</Text>
+          <Text>{netInfo.details?.strength}</Text>
+          <Text>{netInfo.details?.carrier}</Text>
+          <Text>{netInfo.details?.cellularGeneration}</Text>
+          <Text>{netInfo.details?.frequency}</Text>
+          {netInfo.details?.isConnectionExpensive !== undefined && (
+            <Text>{netInfo.details?.isConnectionExpensive.toString()}</Text>
+          )}
+        </View>
+      </View>
+      
     </SafeAreaView>
   );
 };
 
 export default DashBoard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  dashboard: {
+    flex: 1,
+    flexDirection: "row",
+    marginHorizontal: 30,
+    marginVertical: 160,
+    padding: 20,
+
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    borderWidth: 2,
+    borderColor: 'lightseagreen',
+    borderRadius: 20
+  },
+  firstColumn: {
+    flex: 1,
+    alignItems: "flex-end"
+  },
+  secondColumn: {
+    flex: 1,
+    
+  }
+});
