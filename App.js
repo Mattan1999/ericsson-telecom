4e37e0f7b3ce60ed7eb5262c8a6850a1434374fa
Mattan1999 <mattias470@gmail.com>
2021-11-25 13:32:35 +0100

    Fixing imports

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-// import 'react-native-gesture-handler';
+// import "react-native-gesture-handler";
 
 import React from "react";
 
-import TabBar from './src/navigation/TabBar';
+import TabBar from "./src/navigation/TabBar";
 
 export default function App() {
-  return (
-    <TabBar />
-  );
+  return <TabBar />;
 }
